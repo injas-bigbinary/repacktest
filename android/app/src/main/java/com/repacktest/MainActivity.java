454ca25f1e1ce36cc8cd182583831d7000e96d94
INJAS MTP <injas.mtp@bigbinary.com>
2021-09-03 13:36:30 +0530

    first commit

---
+++ android/app/src/main/java/com/repacktest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.repacktest;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "repacktest";
+  }
+}
